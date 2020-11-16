da321d1b5cf9db56adfcbc98d41f7e6d2713bfc0
Yarden Lawson <yardenlawson@gmail.com>
2020-11-16 21:18:08 +0000

    backend controllers

--- models/donation.js
+++ models/donation.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose')
 const donationsSchema = new mongoose.Schema({
   donation_date: { type: Date, default: Date.now, required: true },
   donor_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Donor', required: true },
-  donation_amount: { type: Number, required: true }
+  donation_amount: { type: Number, required: true },
+  donation_period_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Period', required: true }
 }, {
   timestamps: true
 })
 
 
 module.exports = mongoose.model('Donations', donationsSchema)
+
+
---
+++ models/donationPeriod.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const donationPeriodSchema = new mongoose.Schema({
+    donor_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Donor', required: true },
+    active: { type: Boolean },
+    donation_details: {
+        amount: { type: Number, required: true },
+        method: { type: String, required: true, enum: ['equal', 'more-odd'] },
+        period: { type: Number, required: true, default: 10 },
+        start_date: { type: Date, default: Date.now }
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Period', donationPeriodSchema)
