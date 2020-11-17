52b07cd1ebd09300da54186ea087c8f3e8cc6810
Yarden Lawson <yardenlawson@gmail.com>
2020-11-17 17:28:02 +0000

    backend controllers

--- controllers/webhooks.js
+++ controllers/webhooks.js
@@ -21,21 +21,31 @@ const createDonation = asyncHandler(async (req, res, next) => {
 
 
   for (let i = 0; i < allPeriods.length; i++) {
+    // RETURNING THE DIFFERENCE AS A NUMBER BETWEEN TWO DATES - TODAY AND START DATE
+    const periodDifference = Math.floor((moment() - allPeriods[i].donation_details.start_date) / (1000 * 60 * 60 * 24))
     // CHECKING IF DATE NOW MINUS DONATION START DATE IS GREATER THAN DONATION PERIOD
     // IF TRUE TURN DONATION TO FALSE
-    if (dateNow - allPeriods[i].donation_details.start_date > allPeriods[i].donation_details.period) {
+    if (periodDifference > allPeriods[i].donation_details.period) {
       await Period.findByIdAndUpdate(allPeriods[i]._id, { active: false })
       console.log('donation date has expired, changing active to false')
     } else {
-      query = Donation.findOne({ donation_period_id: allPeriods[i]._id })
-      const donation = await query
+      query = Donation.find({ donation_period_id: allPeriods[i]._id })
+      const donations = await query
+
+      for (let ii = 0; ii < donations.length; ii++) {
+        if (moment(donations.donation_date, 'YYYY-MM-DD') >= )
+      }
+    
       // DOES THE DONATION DATE MATCH TODAY'S DATE
+      // IF donation IS NULL
       // IF DONATION DATE DOES NOT MATCH TODAY'S DATE
-      if (moment(donation.donation_date).format('YYYY-MM-DD') !== moment().format('YYYY-MM-DD')) {
+      if (donation === null ) {
         // PREPPING donation OBJECT TO BE CREATED
+        const toPay = await calcDonationPayment(allPeriods[i], periodDifference)
+        console.log('toPay', toPay)
         const newDonation = {
           donor_id: allPeriods[i].donor_id,
-          donation_amount: allPeriods[i].donation_details.amount,
+          donation_amount: toPay,
           donation_period_id: allPeriods[i]._id
         }
         // IF NOT DONATION HAS BEEN MADE TODAY = CREATING A NEW DONATION
@@ -52,9 +62,41 @@ const createDonation = asyncHandler(async (req, res, next) => {
 })
 
 
+const calcDonationPayment = async (period, periodDifference) => {
+  let toPay = 0
+  let query
+  if (period.donation_details.method === 'equal') {
+    toPay = period.donation_details.amount / 10
+  } else if (period.donation_details.method === 'more-odd' && periodDifference !== 0) {
+    if (periodDifference % 2 === 0) {
+      console.log('even')
+      toPay = ((period.donation_details.amount * 1 / 3) / (period.donation_details.period / 2)).toFixed(2) // 2 decimal places
+    } else if (periodDifference % 2 === 1) {
+      console.log('odd')
+      toPay = ((period.donation_details.amount * 2 / 3) / (period.donation_details.period / 2)).toFixed(2)  // 2 decimal places
+    }
+    // IF ITS LAST DAY AND THERE IS AN AMOUNT REMAINING
+  } else if (period.donation_details.method === 'more-odd' && periodDifference === 0) {
+    // ALL DONATIONS IN A SINGLE GIVEN PERIOD OF TIME
+    query = Donation.find({ donation_period_id: period._id })
 
+    const allPeriodDonations = await query
+
+    // COUNTING ALL DONATIONS THAT HAVE BEEN MADE 
+    let hasBeenPaid = 0
+    for (let i = 0; i < allPeriodDonations.length; i++) {
+      hasBeenPaid += allPeriodDonations[i].donation_amount 
+    }
+    // RETURNING ORIGINAL DONATION AMOUNT - WHAT HAS BEEN PAID = DIFFERENCE
+    toPay = period.donation_details.amount - hasBeenPaid
+  }
+  return toPay
+}
 
 
 module.exports = {
   createDonation
 }
+
+
+
