dc09912bcaff1c684cb274d5347bc3b3c9d8c33a
Yarden Lawson <yardenlawson@gmail.com>
2020-11-18 16:12:29 +0000

    frontend form

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -9,49 +9,19 @@ const Main = () => {
   const [donorsState, setDonorsState] = useState([])
   const [isLoading, setIsLoading] = useState(true)
 
-  useEffect(() => {
-    const fetchDonors = async () => {
-      try {
-        const res = await getAllPeriods()
-        console.log('all periods', res)
-        let periods = []
-        res.data.map(period => {
-          periods.push({
-            periodId: period._id
-          })
-        })
-        setDonorsState(periods)
-        setIsLoading(false)
-      } catch (err) {
-        console.log(err)
-      }
-    }
-      fetchDonors()
-
-  }, [])
-
   const handleNewDonor = (newDonor) => {
     let newDonorsArray = [...donorsState]
 
     console.log('newDonor', newDonor)
 
-    newDonorsArray.push({
-      // donorId: newDonor._id,
-      name: newDonor.name,
-      email: newDonor.email,
-      payment_method: {
-        name_on_card: newDonor.name_on_card,
-        card_number: newDonor.card_number,
-        security_number: newDonor.security_number,
-        expiration_date: newDonor.expiration_date
-      }
-    })
+    newDonorsArray.push(newDonor)
+    
     setDonorsState(newDonorsArray)
   }
 
   return (
     <div>
-      <h1>hello</h1>
+      <h1>Charity</h1>
       <NewDonor handleNewRover={handleNewDonor} />
     </div>
 
